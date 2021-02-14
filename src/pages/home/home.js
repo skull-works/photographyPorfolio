7de46cf2f3d42136df2b9886ec3b9b14b0881f0c
mabutalid <theoaguilar123@gmail.com>
2021-02-14 13:26:37 +0800

    applied navigation layout

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -8,15 +8,31 @@ import React from 'react';
 
 const Home = () => {
     return(
-        <>  
+        <div className='bg-lightgreen'>  
             <title>Gualbertsan Sual</title>
-            <header className='h-12vh border-4 border-gray-300'>
+            <header className='h-12vh'>
+                <nav className='w-11/12 h-full mx-auto relative'>
+                    <ul className='flex flex-row absolute bottom-0'>
+                        <li className='ml-2 px-4 border-r-2 border-gray-300 font-Raleway font-semibold text-gray-300'>
+                            BIO
+                        </li>
+                        <li className='ml-2 px-4 border-r-2 border-gray-300 font-Raleway font-semibold text-gray-300'>
+                            WORKS
+                        </li>
+                        <li className='ml-2 px-4 border-r-2 border-gray-300 font-Raleway font-semibold text-gray-300'>
+                            EXHIBIT
+                        </li>
+                        <li className='ml-2 px-4 border-r-2 border-gray-300 font-Raleway font-semibold text-gray-300'>
+                            CONTACT
+                        </li>
+                    </ul>
+                </nav>
             </header>
             <main className='content h-70vh border-b-4 border-red-300'>
             </main>
             <footer className='h-18vh border-t-4 border-green-300'> 
             </footer>
-        </>
+        </div>
     )
 }
 
